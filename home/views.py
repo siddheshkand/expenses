27770031e11edd63d9df3c22d90cf79668e4c0f1
siddkand <siddkand@gmail.com>
2019-07-03 20:50:00 +0530

    avg median added

--- home/views.py
+++ home/views.py
@@ -1,10 +1,39 @@
+import datetime
+
 from django.contrib.auth.decorators import login_required
 from django.db.models import Sum, Q
 from django.forms import formset_factory
 from django.shortcuts import redirect, render
-from django.views.generic import CreateView
+# from django.views.generic import CreateView
 
 from . import forms, models
+import statistics
+
+
+def get_stat():
+    stats = {}
+    if models.IncomeAndExpense.objects.exists():
+        start_date = models.IncomeAndExpense.objects.earliest('date').date
+        end_date = models.IncomeAndExpense.objects.latest('date').date
+        # Iterate over start end
+        day = datetime.timedelta(days=1)
+        daily_expenses_array = []
+        while start_date <= end_date:
+            daily_sum = models.IncomeAndExpense.objects.filter(type='expense').filter(date=start_date).aggregate(
+                Sum('amount'))['amount__sum']
+            if not daily_sum:
+                daily_sum = 0
+            daily_expenses_array.append(daily_sum)
+            start_date = start_date + day
+        daily_expenses_median = statistics.median(daily_expenses_array)
+        daily_expenses_mean = statistics.mean(daily_expenses_array)
+        daily_expenses_mode = statistics.mode(daily_expenses_array)
+        # print(daily_expenses_array)
+        stats.update({'daily_expenses_mean': int(daily_expenses_mean)})
+        stats.update({'daily_expenses_median': int(daily_expenses_median)})
+        stats.update({'daily_expenses_mode': int(daily_expenses_mode)})
+
+        return stats
 
 
 @login_required(login_url='/admin/login/')
@@ -36,6 +65,9 @@ def home(request):
     bank_balance = bank_income_total - bank_expense_total
     cash_balance = cash_income_total - cash_expense_total
     total = bank_balance + cash_balance
+
+    stats = get_stat()
+
     context = {
         'form': form,
         'incomeAndExpense': incomeAndExpense,
@@ -44,6 +76,7 @@ def home(request):
         'bank_balance': bank_balance,
         'cash_balance': cash_balance,
         'total': total,
+        'stats': stats
     }
     return render(request, 'home/home.html', context)
 
