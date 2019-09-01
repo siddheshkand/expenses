8bdb5bb899a18b1c0aaff2deb4b97ee070eb39b1
siddkand <siddkand@gmail.com>
2019-09-01 14:40:43 +0530

    features added

--- home/urls.py
+++ home/urls.py
@@ -8,9 +8,10 @@ from . import views
 app_name = 'home'
 
 urlpatterns = [
-                  path('', views.home, name='home'),
-                  path('get/transaction/', views.send_transaction_in_json, name='send_transaction'),
-                  path('expense/create/multiple/', views.expenses_create_formset, name='expense_create_multiple'),
-                  path('income/create/multiple/', views.income_create_formset, name='income_create_multiple'),
-                  path('schedule/create/multiple/', views.schedule_create_formset, name='schedule_create_multiple'),
-              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('', views.home, name='home'),
+    path('get/transaction/', views.send_transaction_in_json, name='send_transaction'),
+    path('expense/create/multiple/', views.expenses_create_formset, name='expense_create_multiple'),
+    path('income/create/multiple/', views.income_create_formset, name='income_create_multiple'),
+    path('schedule/create/multiple/', views.schedule_create_formset, name='schedule_create_multiple'),
+]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
