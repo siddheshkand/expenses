cbea4a8eccb42080bac3059af8ff2d4d32258cd2
siddkand <siddkand@gmail.com>
2019-09-01 14:42:25 +0530

    features added

--- expenses/settings.py
+++ expenses/settings.py
@@ -118,7 +118,7 @@ DATE_INPUT_FORMATS = [
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+#
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
