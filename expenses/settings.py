ca703d53ffb49f1d962cdafe27d1dcd82b1afe2b
siddheshkand <siddkand@gmail.com>
2019-09-01 10:12:59 +0000

    Recover attempt

--- expenses/settings.py
+++ expenses/settings.py
@@ -118,8 +118,16 @@ DATE_INPUT_FORMATS = [
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+<<<<<<< Updated upstream
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
 #
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, "static"),
 # ]
+=======
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+>>>>>>> Stashed changes
